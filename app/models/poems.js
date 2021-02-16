041cbf383913e692b5ee130fd87c9cc00c1c1570
desertidaho <wilcox.brett@gmail.com>
2021-02-16 14:08:41 -0700

    Columns, and reveal CSS

--- app/models/poems.js
+++ app/models/poems.js
@@ -8,22 +8,26 @@ export default class Poems {
     for (let i = 0; i < this.poemsArray.length; i++) {
       template += `
       <div class="cell">
-      <button class="button" onclick="openReveal('#${this.poemsArray[i].id}')" style="background-color: black;">
-        <div class="card" style="border: none; background-color: black; color: white;">
-          <div class="card-section" style="background-image: linear-gradient(black 1px, rgb(38, 38, 38)); padding-bottom: 0px;">
-            <h2 style="font-style: italic; font-family: fantasy;">${this.poemsArray[i].title}</p>
+        <button class="button" onclick="openReveal('#${this.poemsArray[i].id}')" style="background-color: black;">
+          <div class="card" style="border: none; background-color: black; color: white;">
+            <div class="card-section" style="background-image: linear-gradient(black 1px, rgb(38, 38, 38)); padding-bottom: 0px;">
+              <h2 style="font-style: italic; font-family: fantasy;">${this.poemsArray[i].title}</p>
+           </div>
+           <img src="./assets/images/${this.poemsArray[i].img}" alt="poem picture" style="border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; width: 100vw;">
           </div>
-          <img src="./assets/images/${this.poemsArray[i].img}" alt="poem picture" style="border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; width: 100vw;">
-        </div>
-      </button>
+        </button>
       </div>
 
-      <div class="reveal tiny" id="${this.poemsArray[i].id}" data-reveal style="color: black;">
-        <h1 style="font-style: italic; font-family: fantasy; margin-top: 40px;">${this.poemsArray[i].title}</h1>
-        <p style="font-style: italic; margin-top: 1rem; margin-bottom: 3rem; font-size: 0.7rem;">${this.poemsArray[i].poem}</p>
-        <button class="close-button" data-close aria-label="Close modal" type="button">
-         <span aria-hidden="true">&times;</span>
-        </button>
+      <div class="reveal small" id="${this.poemsArray[i].id}" data-reveal style="color: black;">
+        <div class="grid-x grid-margin-x align-center" style="margin-top: 40px;">
+            <h1 style="font-style: italic; font-family: fantasy;">${this.poemsArray[i].title}</h1>
+        </div>
+        <div class="grid-x grid-margin-x align-center" style="margin-top: 10px;">
+          <p style="font-style: italic; margin-top: 1rem; margin-bottom: 3rem; font-size: 0.7rem;">${this.poemsArray[i].poem}</p>
+          <button class="close-button" data-close aria-label="Close modal" type="button">
+          <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
 
         <div class="grid-x grid-margin-x align-center align-spaced" style="font-size: 1.5rem;"">
           <a class="warning button" href="https://www.amazon.com/gp/product/1792611633" target="_blank" aria-labelledby="Amazon" style="color: Black;">Buy on Amazon</a>
