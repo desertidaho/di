16b0d148432260366d2f835896168cc892424a08
desertidaho <wilcox.brett@gmail.com>
2021-02-16 14:51:18 -0700

    Make cards all same height

--- app/models/poems.js
+++ app/models/poems.js
@@ -13,7 +13,7 @@ export default class Poems {
             <div class="card-section" style="background-image: linear-gradient(black 1px, rgb(38, 38, 38)); padding-bottom: 0px;">
               <h2 style="font-style: italic; font-family: fantasy;">${this.poemsArray[i].title}</p>
            </div>
-           <img src="./assets/images/${this.poemsArray[i].img}" alt="poem picture" style="border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; width: 100vw;">
+           <img src="./assets/images/${this.poemsArray[i].img}" alt="poem picture" style="border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; width: 100vw; height: 400px; object-fit: cover;">
           </div>
         </button>
       </div>
