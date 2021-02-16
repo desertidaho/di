adc5e13079c68335d9098f2ccc65fb36f2d5d3aa
desertidaho <wilcox.brett@gmail.com>
2021-02-15 18:43:56 -0700

    Looking pretty good.

--- app/models/poems.js
+++ app/models/poems.js
@@ -12,7 +12,7 @@ export default class Poems {
           <div class="card-section" style="background-image: linear-gradient(black 2px, rgb(77, 75, 75)); padding-bottom: 0px;">
             <h2 style="font-style: italic; font-family: fantasy;">${this.poemsArray[i].title}</p>
           </div>
-          <img src="./assets/images/${this.poemsArray[i].img}" alt="poem picture" style="border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
+          <img src="./assets/images/${this.poemsArray[i].img}" alt="poem picture" style="border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; width: 100vw;">
         </div>
       </button>
 
